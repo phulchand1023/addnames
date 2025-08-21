218d8170061001f2955278892e08da8264c5c2f8
Phulchand Kumar <158141215+phulchand1023@users.noreply.github.com>
2025-08-21 14:02:15 +0530

    Updated the config for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://addnames.onrender.com", 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // default, but explicit
+  },
 });
